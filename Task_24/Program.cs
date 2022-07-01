d3ebbbe0fe5832db83dacbccf879938f5a120f49
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2022-07-01 11:08:33 +0300

    Add new filel

---
+++ Task_24/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+// for (int i = 1; i <= number; i++)
+
+// {
+// sum = i + sum; // sum += i;
+// if( i < number) Console.Write($"{i} + ");
+// else Console.Write($"{i} = ");
+// }
+// Console.WriteLine(sum);
+
+int Sum(int a)
+{
+    int sum1 = 0;
+ for (int i = 1; i <= a; i++)
+ 
+    {
+     sum1 = i + sum1;
+   }
+ return sum1;
+}
+ sum = Sum(number);
+ int sum2 = Sum(5);
+ Console.WriteLine(sum2);
+Console.WriteLine($"Сумма числе от 1 до {number} = {sum}");
---
+++ Task_26-1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (num != 0)
+{
+    num = num / 10;
+    count = count + 1;
+}
+Console.WriteLine($" Количество символов в массиве =  {count}");
